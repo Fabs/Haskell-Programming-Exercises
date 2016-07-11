7798aedd0f7eda58221b493e717718bf95f824de
David <daveparrish@gmail.com>
2016-07-10 23:49:57 -0400

    Add stop_vowels combinations using liftA3

---
+++ Applicative/stops_vowels.hs
@@ -0,0 +1,9 @@
+import Control.Applicative (liftA3)
+
+stops :: String
+stops = "pbtdkg"
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a,b,c)]
+combos as bs cs = liftA3 (\x y z -> (x,y,z)) as bs cs
