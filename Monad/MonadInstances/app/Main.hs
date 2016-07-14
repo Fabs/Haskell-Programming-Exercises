bdb4c3b58b053707b7a6819c32972c66378d1353
David <daveparrish@gmail.com>
2016-07-14 12:13:52 -0400

    Add Functor for PhhhbbtttEither type

--- Monad/MonadInstances/app/Main.hs
+++ Monad/MonadInstances/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment
 import Sum
 import Nope
+import PhhhbbtttEither
 
 main :: IO ()
 main = getArgs >>= checkBasedOnArgs
@@ -10,6 +11,7 @@ main = getArgs >>= checkBasedOnArgs
 checkBasedOnArgs :: [String] -> IO ()
 checkBasedOnArgs ("Sum":_) = checkSum
 checkBasedOnArgs ("Nope":_) = checkNope
+checkBasedOnArgs ("PhhhbbtttEither":_) = checkPhhhbbtttEither
 checkBasedOnArgs (x:_) = do
   putStrLn $ "First argument '" ++ x ++ "' didn't match a check."
-  putStrLn $ "Try one of the following:\n Sum\n Nope"
+  putStrLn $ "Try one of the following:\n Sum\n Nope\n PhhhbbtttEither"
