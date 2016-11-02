c5155a19e72208bf60d4ecce5118f98e0b174504
David Parrish <daveparrish@tutanota.com>
2016-11-02 11:14:01 -0400

    Add solution for 'Exercises: A Head Code'

---
+++ Chapter 2/Exercises: A Head Code.hs
@@ -0,0 +1,23 @@
+{--
+1.
+
+Prelude> let x = 5 in x
+5
+
+2.
+
+Prelude> let x = 5 in x * x
+25
+
+3.
+
+Prelude> let x = 5; y = 6 in x * y
+30
+
+4.
+
+Prelude> let x = 3; y = 1000 in x + 3
+6
+--}
+
+main = putStrLn "See comments"
