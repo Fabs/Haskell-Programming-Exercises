a99ad0b5b2098ca29eb29ff04c3412706e34e913
David Parrish <daveparrish@tutanota.com>
2017-11-26 10:38:16 -0500

    Add Vigenère cipher exercise

--- Chapter 9/Cipher.hs
+++ Chapter 9/Cipher.hs
@@ -41,3 +41,39 @@ unCeaser :: Int    -- Number of left shifts
          -> String -- Encoded message
          -> String -- Original message
 unCeaser = ceaser . negate
+
+
+-- Chapter 11 Vigenère cipher
+
+{- Not really a very clever way of encoding and decoding but it works
+
+Also, doesn't encode the same way as described in the exercise.
+
+λ vEncode (VKeyword "ally") (VPlain "meetatdawn")
+VCipher "fiiktxhrpr"
+
+λ vDecode (VKeyword "ally") (VCipher "fiiktxhrpr")
+VPlain "meetatdawn"
+
+-}
+
+
+newtype VKeyword  = VKeyword String deriving Show
+newtype VPlain    = VPlain  String deriving Show
+newtype VCipher   = VCipher String deriving Show
+
+vEncode :: VKeyword -> VPlain -> VCipher
+vEncode (VKeyword []) (VPlain plain)  = VCipher plain
+vEncode (VKeyword key) (VPlain plain) = VCipher (vEncode' key plain)
+  where
+    vEncode' _      []     = []
+    vEncode' []     plain' = vEncode' key plain'
+    vEncode' (k:ks) (p:ps) = (ceaserShift (ord k) p):(vEncode' ks ps)
+
+vDecode:: VKeyword -> VCipher -> VPlain
+vDecode (VKeyword []) (VCipher cipher)  = VPlain cipher
+vDecode (VKeyword key) (VCipher cipher) = VPlain (vEncode' key cipher)
+  where
+    vEncode' _      []     = []
+    vEncode' []     cipher' = vEncode' key cipher'
+    vEncode' (k:ks) (c:cs) = (ceaserShift (negate $ ord k) c):(vEncode' ks cs)
